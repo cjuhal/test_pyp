0d5ea6529e0281876ecb53993bac2f6927384a9f
micaela quijano <micaquijano04@gmail.com>
2022-01-11 14:52:06 -0300

    proyecto nuevo

--- wp-content/themes/salient/header.php
+++ wp-content/themes/salient/header.php
@@ -268,7 +268,7 @@ if($perm_trans != 1 || $perm_trans == 1 && $bg_header == 'false' || $page_full_s
 	}
 ?>
 <!-- MENU -->
-<?php 
+<?php echo "casa";
 	echo "<nav class='navbar navbar-expand-lg navbar-dark bg-teal config_menu'>
 	<button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarsExample08' aria-controls='navbarsExample08' aria-expanded='false' aria-label='Toggle navigation'>
 		<span class='navbar-toggler-icon'></span>
