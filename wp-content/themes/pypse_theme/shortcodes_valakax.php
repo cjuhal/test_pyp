d477598fd149b3d988aecbc0a8d73759f9b07a4f
Christian Juhal <chrisjuhal@gmail.com>
2022-01-15 21:21:01 -0300

    sub options con boostrap

--- wp-content/themes/pypse_theme/shortcodes_valakax.php
+++ wp-content/themes/pypse_theme/shortcodes_valakax.php
@@ -590,7 +590,14 @@ if ( !function_exists( 'valakax_custom_nav_menu' ) ) {
 		class Valakax_Custom_Walker_Nav_Menu extends Walker_Nav_Menu {
 
 			function start_el(&$output, $item, $depth=0, $args=[], $id=0) {
-				$output .= "<li class='" .  implode(" ", $item->classes) . "'>";
+				$item_class = $item->classes;
+				if($args->show_carets && $args->walker->has_children){
+					$clases_parent_child = ["nav-link dropdown-toggle"];
+					array_push($item_class, $item->classes, $clases_parent_child);
+					$attr_parent_child = 'role="button" data-bs-toggle="dropdown" aria-expanded="false"';
+				}
+
+				$output .= "<li class='" .  implode(" ", $item->classes) . "'". $attr_parent_child."id='navbarDropdown".$id."'>";
 		 
 				if ($item->url && $item->url != '#') {
 					$output .= '<a href="' . $item->url . '">';
@@ -611,6 +618,46 @@ if ( !function_exists( 'valakax_custom_nav_menu' ) ) {
 
 
 			}
+			
+			/**
+	 * Starts the list before the elements are added.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @see Walker::start_lvl()
+	 *
+	 * @param string   $output Used to append additional content (passed by reference).
+	 * @param int      $depth  Depth of menu item. Used for padding.
+	 * @param stdClass $args   An object of wp_nav_menu() arguments.
+	 */
+	function start_lvl( &$output, $depth = 0, $args = null ) {
+		if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+			$t = '';
+			$n = '';
+		} else {
+			$t = "\t";
+			$n = "\n";
+		}
+		$indent = str_repeat( $t, $depth );
+
+		// Default class.
+		$classes = array( 'sub-menu', 'dropdown-menu' );
+
+		/**
+		 * Filters the CSS class(es) applied to a menu list element.
+		 *
+		 * @since 4.8.0
+		 *
+		 * @param string[] $classes Array of the CSS classes that are applied to the menu `<ul>` element.
+		 * @param stdClass $args    An object of `wp_nav_menu()` arguments.
+		 * @param int      $depth   Depth of menu item. Used for padding.
+		 */
+		$class_names = implode( ' ', apply_filters( 'nav_menu_submenu_css_class', $classes, $args, $depth ) );
+		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+		$output .= "{$n}{$indent}<ul$class_names aria-labelledby='navbarDropdown'".$args->id.">{$n}";
+	}
+
 
 		}
 
